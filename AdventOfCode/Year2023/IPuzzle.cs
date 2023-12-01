dcf0b4f93f13a2efe581cca2d76c3f20343381a6
Annamária Szentpéteri <szaea1994@gmail.com>
2023-12-01 13:58:18 +0100

    Add base code and first input

---
+++ AdventOfCode/Year2023/IPuzzle.cs
@@ -0,0 +1,7 @@
+﻿namespace Year2023
+{
+    public interface IPuzzle<T>
+    {
+        public T SolvePuzzle();
+    }
+}
---
+++ AdventOfCode/Year2023/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Year2023;
+
+try
+{
+    var success = false;
+
+    while (!success)
+    {
+        Console.WriteLine("Please provide which puzzle's result you want to see! (number of puzzle)");
+        success = int.TryParse(Console.ReadLine(), out var puzzleNumber);
+
+        if (!success)
+        {
+            Console.WriteLine("Invalid input.");
+            continue;
+        }
+
+        switch (puzzleNumber)
+        {
+            case 1:
+                {
+                    var puzzle = new Puzzle01("input01.txt");
+                    var solution = puzzle.SolvePuzzle();
+                    Console.WriteLine($"Solution for puzzle #{puzzleNumber} is {solution}");
+                    break;
+                }
+            default:
+                {
+                    Console.WriteLine("Invalid input. Number must be between 1-24.");
+                    success = false;
+                    break;
+                }
+        }
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine("Unexpected error happened:");
+    Console.WriteLine(e);
+}
