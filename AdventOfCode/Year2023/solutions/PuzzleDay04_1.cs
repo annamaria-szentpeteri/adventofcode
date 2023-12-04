ae2ab8bb5c0ffecc9265f7d730f47edcf7421555
Annamária Szentpéteri <szaea1994@gmail.com>
2023-12-04 13:41:42 +0100

    Day 4 Puzzle 1 solution

--- AdventOfCode/Year2023/solutions/PuzzleDay04_1.cs
+++ AdventOfCode/Year2023/solutions/PuzzleDay04_1.cs
@@ -12,7 +12,34 @@ internal class PuzzleDay04_1 : IPuzzle
     public int SolvePuzzle()
     {
         var result = 0;
+        
+        using var file = File.OpenText(_inputFileName);
+
+        while (!file.EndOfStream)
+        {
+            var line = file.ReadLine();
+
+            if (string.IsNullOrEmpty(line))
+                continue;
+
+            var winningNumbers = GetWinningNumbers(line);
+
+            if (winningNumbers.Count != 0)
+                result += (int)Math.Pow(2, winningNumbers.Count - 1);
+        }
 
         return result;
+    }
+
+    private static List<string> GetWinningNumbers(string input)
+    {
+        var splitOptions = StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries;
+
+        var cardNumbers = input.Split(':', splitOptions)[1].Split('|', splitOptions);
+
+        var possibleWinningNumbers = cardNumbers[0].Split(' ', splitOptions);
+        var scratchedNumbers = cardNumbers[1].Split(' ', splitOptions);
+
+        return scratchedNumbers.Intersect(possibleWinningNumbers).ToList();
     }
 }
