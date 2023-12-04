716387794e13069ee4e0b6bee70f730440c3c40c
Annamária Szentpéteri <szaea1994@gmail.com>
2023-12-04 13:41:59 +0100

    Day 4 Puzzle 2 solution

--- AdventOfCode/Year2023/solutions/PuzzleDay04_2.cs
+++ AdventOfCode/Year2023/solutions/PuzzleDay04_2.cs
@@ -12,7 +12,64 @@ internal class PuzzleDay04_2 : IPuzzle
     public int SolvePuzzle()
     {
         var result = 0;
+        var cardNumber = 0;
+
+        var extraCopies = new Dictionary<int, int>();
+
+        using var file = File.OpenText(_inputFileName);
+
+        while (!file.EndOfStream)
+        {
+            var line = file.ReadLine();
+
+            if (string.IsNullOrEmpty(line))
+                continue;
+
+            cardNumber++;
+
+            var numberOfCards = 1;
+
+            if (extraCopies.TryGetValue(cardNumber, out var numberOfCopies))
+            {
+                numberOfCards += numberOfCopies;
+            }
+
+            result += numberOfCards;
+
+            var winningNumberCount = GetWinningNumbers(line).Count;
+
+            if (winningNumberCount == 0)
+                continue;
+
+            for (int c = 1; c <= numberOfCards; c++)
+            {
+                for (int i = 1; i <= winningNumberCount; i++)
+                {
+                    var number = cardNumber + i;
+                    if (extraCopies.TryGetValue(number, out var copyCount))
+                    {
+                        extraCopies[number] = copyCount + 1;
+                    }
+                    else
+                    {
+                        extraCopies.Add(number, 1);
+                    }
+                }
+            }
+        }
 
         return result;
+    }
+
+    private static List<string> GetWinningNumbers(string input)
+    {
+        var splitOptions = StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries;
+
+        var cardNumbers = input.Split(':', splitOptions)[1].Split('|', splitOptions);
+
+        var possibleWinningNumbers = cardNumbers[0].Split(' ', splitOptions);
+        var scratchedNumbers = cardNumbers[1].Split(' ', splitOptions);
+
+        return scratchedNumbers.Intersect(possibleWinningNumbers).ToList();
     }
 }
